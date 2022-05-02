ec964a38e7eefcf364bec3e6a4acd20a13db468c
Nick DeRobertis <whoopnip@gmail.com>
2022-05-02 17:13:07 -0400

    chore: Add demo app

--- dev-src/main.ts
+++ dev-src/main.ts
@@ -1,8 +1,9 @@
 import './style.css';
+import '../src/termynal.css';
+import { TerminHTML } from '../src/terminhtml';
 
-const app = document.querySelector<HTMLDivElement>('#app')!;
+function runTerminHTMLApp() {
+  new TerminHTML('#terminhtml-ansi', { initNow: true });
+}
 
-app.innerHTML = `
-  <h1>Hello Vite</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+runTerminHTMLApp();
