dddb9ff4a36b9a17bb011fb475672ea646ab452c
Nick DeRobertis <whoopnip@gmail.com>
2022-05-07 07:26:38 -0400

    chore: Add a manual init dev app

--- dev-src/main.ts
+++ dev-src/main.ts
@@ -13,5 +13,18 @@ function runDelayApp() {
   });
 }
 
+function runManualInitApp() {
+  const term = new TerminHTML("#manual-init");
+  const initButton = document.createElement("button");
+  initButton.innerText = "Init";
+  initButton.style.width = "300px";
+  initButton.style.height = "100px";
+  initButton.addEventListener("click", () => {
+    term.init();
+  });
+  term.container.parentElement!.appendChild(initButton);
+}
+
 runTerminHTMLApp();
 runDelayApp();
+runManualInitApp();
