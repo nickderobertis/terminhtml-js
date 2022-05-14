b32a9a4e5ef5d372359f54504a34cba13d7d11a3
Nick DeRobertis <whoopnip@gmail.com>
2022-05-14 14:42:36 -0400

    chore: Get output in test working

--- test/terminhtml.spec.ts
+++ test/terminhtml.spec.ts
@@ -15,6 +15,8 @@ function createTerminHTMLBlock(): ElemAndTerm {
   const outputSpan = document.createElement("span");
   outputSpan.textContent = "woo";
   pre.appendChild(inputSpan);
+  // Create a text node to force a line break
+  pre.appendChild(document.createTextNode("\n"));
   pre.appendChild(outputSpan);
   const term = new TerminHTML(pre);
   document.body.appendChild(pre);
@@ -52,13 +54,17 @@ describe("TerminHTML", () => {
     await findByText(element, "1x");
     // Check for branding
     await findByText(element, "Created with");
-    // Animation has not run, so output does not exist yet
+    // Animation has not run, so input and output do not exist yet
     expect(queryByText(element, "echo woo")).toBeNull();
+    expect(queryByText(element, "woo")).toBeNull();
 
     await waitForAnimation();
 
     // Check for input displayed
     const inputElem = await findByText(element, "echo woo");
     expect(inputElem.getAttribute("data-ty")).toEqual("input");
+    // Check for output displayed
+    const outputElem = await findByText(element, "woo");
+    expect(outputElem.getAttribute("data-ty")).toEqual(null);
   });
 });
