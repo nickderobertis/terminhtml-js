d73e58e2ba6d103958484690b1c97afe87f371d6
Nick DeRobertis <whoopnip@gmail.com>
2022-05-14 13:29:08 -0400

    chore: Add initial test

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,30 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
+import TerminHTML from "../src";
+import "../src/termynal.css";
 
-describe('index', () => {
-  describe('TerminHTML', () => {
-    it('noop', () => {
-      expect('Hello').toEqual('Hello');
+type ElemAndTerm = {
+  element: HTMLElement;
+  term: TerminHTML;
+};
+
+function createTerminHTMLBlock(): ElemAndTerm {
+  const pre = document.createElement("pre");
+  const inputSpan = document.createElement("span");
+  inputSpan.textContent = "$ echo woo";
+  const outputSpan = document.createElement("span");
+  outputSpan.textContent = "woo";
+  pre.appendChild(inputSpan);
+  pre.appendChild(outputSpan);
+  const term = new TerminHTML(pre);
+  document.body.appendChild(pre);
+  return { element: pre, term };
+}
+
+describe("index", () => {
+  describe("TerminHTML", () => {
+    it("creates a blank, uninitialized terminal", () => {
+      const { element } = createTerminHTMLBlock();
+      expect(element.hasAttribute("data-termynal")).toBe(true);
     });
   });
 });
